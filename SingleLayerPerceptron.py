c0290ea6828e2f8daa630ed0626a4026864dbdc7
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-03-10 22:45:07 +0600

    updated

--- SingleLayerPerceptron.py
+++ SingleLayerPerceptron.py
@@ -19,11 +19,9 @@ dt=[0, 7, 15, 4, 10, 6, 10, 10, 5, 1, 9, 1, 2, 0, 8, 0, 0]
 #print dt
 
                
-w = [0, 7, 5, 4, 10, 6, 10, 10, 5, 1, 9, 1, 2, 0, 8, 0, 0]
-m = []
-delta=[]
-y = []
-s=0    
+w = [0, 9, 17, 6, 2, 8, 12, 7, 3, 1, 9, 10, 20, 2, 8, 2, 2]
+
+   
     
 #for i in range(0,17):
 #    #x.append(rand.randint(0,10))
@@ -33,36 +31,58 @@ s=0
 
     #print y
     #print m
-while (dt!=y):
+def plott(x,y):
+    colors = ['b', 'g']
+    markers = ['o', 'v']
+    plt.plot()
+    plt.xlim([0, 10])
+    plt.ylim([0, 10])
+    plt.title('Dataset')
+    for i in range(0,17):
+        if (x1[i]<=6):
+            plt.plot(x1[i], x2[i], color=colors[0], marker=markers[0],ls='None')
+        else:
+            plt.plot(x1[i], x2[i], color=colors[1], marker=markers[1],ls='None')
+    
+    plt.plot(x1,y)   
+    plt.show()
+
+
+
+a=1
+eta=0.5
+while(a==1):
+    delta=[]
+    y = []
+    s=0 
+    
     for i in range(0,17):
             s+=x1[i]*w[i]+x2[i]*w[i]
             y.append(s)
+    
             #m.append(i)
     for i in range(0,17):
             delta.append(dt[i]-y[i])
-    for i in range(0,17):
-            w[i]=w[i]+delta[i]  
+   # 
+                
+    #for i in range(0,17):
+            if (y[i]==dt[i]):
+                w[i]=w[i]
+            elif (y[i]<dt[i]):
+                w[i]=w[i]+eta*x1[i]
+            else:
+                w[i]=w[i]-eta*x1[i]
+            #w[i]=w[i]+delta[i]     
+    plott(x1,y)
+    
+            
+  
+    #print delta
     
     
-#print delta
-
-
     
-# plot
-colors = ['b', 'g']
-markers = ['o', 'v']
-plt.plot()
-plt.xlim([0, 10])
-plt.ylim([0, 10])
-plt.title('Dataset')
-for i in range(0,17):
-    if (x1[i]<=6):
-        plt.plot(x1[i], x2[i], color=colors[0], marker=markers[0],ls='None')
-    else:
-        plt.plot(x1[i], x2[i], color=colors[1], marker=markers[1],ls='None')
-
-plt.plot(x1,y)   
-plt.show()
-
+        
+    # plot
+    
 
 
